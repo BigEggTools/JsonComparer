9ec1d08546a7ff5ce05140419f47916898f6fbf0
BigEgg <cd_bigegg@hotmail.com>
2017-01-22 20:14:34 +0800

    Add base Initializes & Finalizes function for parser

--- BigEgg.ConsoleExtension/Parameters/Parser.cs
+++ BigEgg.ConsoleExtension/Parameters/Parser.cs
@@ -1,12 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BigEgg.ConsoleExtension.Parameters
+﻿namespace BigEgg.ConsoleExtension.Parameters
 {
-    public class Parser
+    using System;
+
+    /// <summary>
+    /// The parser to parse the console arguments
+    /// </summary>
+    public class Parser : IDisposable
     {
+        private static readonly Lazy<Parser> DefaultParser = new Lazy<Parser>(
+            () => new Parser(ParserSettings.Builder().WithDefault().Build()));
+        private readonly ParserSettings settings;
+        private bool disposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Parser"/> class with a specific setting, <seealso cref="ParserSettings"/>.
+        /// </summary>
+        /// <param name="settings">The parser settings.</param>
+        public Parser(ParserSettings settings)
+        {
+            this.settings = settings;
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="Parser"/> class.
+        /// </summary>
+        ~Parser()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Gets the singleton instance created with default settings.
+        /// </summary>
+        public static Parser Default
+        {
+            get { return DefaultParser.Value; }
+        }
+
+
+
+        /// <summary>
+        /// Releases managed resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposed) return;
+
+            if (disposing)
+            {
+                if (settings != null)
+                    settings.Dispose();
+
+                disposed = true;
+            }
+        }
     }
 }
