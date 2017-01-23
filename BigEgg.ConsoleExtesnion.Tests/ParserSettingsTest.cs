d06533866cabe154085629259f21272d04c2d341
BigEgg <cd_bigegg@hotmail.com>
2017-01-23 18:46:01 +0800

    Add tests for ParserSettings model

---
+++ BigEgg.ConsoleExtesnion.Tests/ParserSettingsTest.cs
@@ -0,0 +1,40 @@
+﻿namespace BigEgg.ConsoleExtesnion.Tests
+{
+    using System.IO;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using BigEgg.ConsoleExtension.Parameters;
+
+    [TestClass]
+    public class ParserSettingTest
+    {
+        [TestMethod]
+        public void DisposeTest_NotDisposeHelpWriter()
+        {
+            using (var helpWriter = new MockStringWriter())
+            {
+                using (var settings = ParserSettings.Builder().WithDefault().HelpWriter(helpWriter).Build())
+                {
+                    Assert.IsFalse(helpWriter.Disposed);
+                }
+                Assert.IsFalse(helpWriter.Disposed);
+            }
+        }
+
+        public class MockStringWriter : StringWriter
+        {
+            public MockStringWriter()
+            {
+                Disposed = false;
+            }
+
+            public bool Disposed { get; private set; }
+
+            protected override void Dispose(bool disposing)
+            {
+                Disposed = true;
+                base.Dispose(disposing);
+            }
+        }
+    }
+}
