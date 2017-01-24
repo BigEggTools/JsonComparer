f53b7142e7136c44df9c39e80c2f1db5724b6f6f
BigEgg <cd_bigegg@hotmail.com>
2017-01-24 17:51:08 +0800

    Add assembly header attribute to have the assembly customized header message

---
+++ BigEgg.ConsoleExtension/Parameters/Attributes/AssemblyHeaderAttribute.cs
@@ -0,0 +1,31 @@
+﻿namespace BigEgg.ConsoleExtension.Parameters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// The assembly header attribute
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, Inherited = false)]
+    [ComVisible(false)]
+    public class AssemblyHeaderAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssemblyHeaderAttribute"/> class.
+        /// </summary>
+        /// <param name="lines">The lines of the header text.</param>
+        public AssemblyHeaderAttribute(params string[] lines)
+        {
+            Lines = lines;
+        }
+
+        /// <summary>
+        /// Gets the lines of header text.
+        /// </summary>
+        /// <value>
+        /// The lines.
+        /// </value>
+        public IEnumerable<string> Lines { get; private set; }
+    }
+}
