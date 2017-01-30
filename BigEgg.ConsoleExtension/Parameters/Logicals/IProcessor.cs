3a28ba3d4644bdbcb52b78ced0b095dfdcf1d863
BigEgg <cd_bigegg@hotmail.com>
2017-01-30 23:16:04 +0800

    Add the processor to handle the version token

---
+++ BigEgg.ConsoleExtension/Parameters/Logicals/IProcessor.cs
@@ -0,0 +1,13 @@
+﻿namespace BigEgg.ConsoleExtension.Parameters.Logicals
+{
+    using System;
+    using System.Collections.Generic;
+
+    using BigEgg.ConsoleExtension.Parameters.Results;
+    using BigEgg.ConsoleExtension.Parameters.Tokens;
+
+    internal interface IProcessor
+    {
+        ParserResult Process(IList<Token> tokens, IList<Type> types, bool CaseSensitive);
+    }
+}
---
+++ BigEgg.ConsoleExtension/Parameters/Logicals/VersionProcessor.cs
@@ -0,0 +1,25 @@
+﻿namespace BigEgg.ConsoleExtension.Parameters.Logicals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using BigEgg.ConsoleExtension.Parameters.Results;
+    using BigEgg.ConsoleExtension.Parameters.Tokens;
+    using BigEgg.ConsoleExtension.Parameters.Errors;
+
+    internal class VersionProcessor : IProcessor
+    {
+        public ParserResult Process(IList<Token> tokens, IList<Type> types, bool CaseSensitive)
+        {
+            if (tokens == null) { throw new ArgumentNullException("tokens"); }
+            if (types == null) { throw new ArgumentNullException("types"); }
+
+            if (tokens.Any(t => t.TokenType == TokenType.Version))
+            {
+                return new ParseFailedResult(new List<Error>() { new VersionRequestError() });
+            }
+            return null;
+        }
+    }
+}
