e78a480966c57d84cfe8bfd102aa29742c495c75
BigEgg <cd_bigegg@hotmail.com>
2017-01-30 23:13:09 +0800

    Update the Command Help Request Error to get the value

--- BigEgg.ConsoleExtension/Parameters/Errors/CommandHelpRequestError.cs
+++ BigEgg.ConsoleExtension/Parameters/Errors/CommandHelpRequestError.cs
@@ -4,15 +4,19 @@
 
     internal class CommandHelpRequestError : Error
     {
-        public CommandHelpRequestError()
+        public CommandHelpRequestError(string commandName, bool existed, Type commandType)
             : base(ErrorType.CommandHelpRequest, true)
-        { }
+        {
+            CommandName = commandName;
+            Existed = existed;
+            CommandType = existed ? commandType : null;
+        }
 
 
         public string CommandName { get; private set; }
 
-        public bool Existed { get; set; }
+        public bool Existed { get; private set; }
 
-        public Type CommandType { get; set; }
+        public Type CommandType { get; private set; }
     }
 }
