d6211a46efd4e84fa0561031b58640009baea9b6
BigEgg <cd_bigegg@hotmail.com>
2017-02-06 13:39:21 +0800

    Add tests for reflection helper

---
+++ BigEgg.ConsoleExtension.Tests/Parameters/FakeParameters/GitClone.cs
@@ -0,0 +1,11 @@
+﻿namespace BigEgg.ConsoleExtension.Tests.Parameters.FakeParameters
+{
+    using BigEgg.ConsoleExtension.Parameters;
+
+    [Command("Clone", "Clone a repository into a new directory")]
+    public class GitClone
+    {
+        [StringProperty("repository", "rep", "The (possibly remote) repository to clone from. See the URLS section below for more information on specifying repositories.", Required = true)]
+        public string Repository { get; set; }
+    }
+}
---
+++ BigEgg.ConsoleExtension.Tests/Parameters/Utils/ReflectionHelperTest.cs
@@ -0,0 +1,43 @@
+﻿namespace BigEgg.ConsoleExtension.Tests.Parameters.Utils
+{
+    using System.Reflection;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using BigEgg.ConsoleExtension.Parameters.Utils;
+
+    using BigEgg.ConsoleExtension.Tests.Parameters.FakeParameters;
+
+    [TestClass]
+    public class ReflectionHelperTest
+    {
+        [TestMethod]
+        public void GetAssemblyTest()
+        {
+            var assembly = ReflectionHelper.GetAssembly();
+            Assert.IsNotNull(assembly);
+        }
+
+        [TestMethod]
+        public void GetAssemblyAttributeTest()
+        {
+            var attribute = ReflectionHelper.GetAssemblyAttribute<AssemblyProductAttribute>();
+            Assert.IsNotNull(attribute);
+            Assert.AreEqual("BigEgg.Tools", attribute.Product);
+        }
+
+        [TestMethod]
+        public void GetCommandTest_NoCommand()
+        {
+            var command = ReflectionHelper.GetCommand(typeof(EmptyClass));
+            Assert.IsNull(command);
+        }
+
+        [TestMethod]
+        public void GetCommandTest_HasCommand()
+        {
+            var command = ReflectionHelper.GetCommand(typeof(GitClone));
+            Assert.IsNotNull(command);
+            Assert.AreEqual("Clone", command.Name);
+        }
+    }
+}
