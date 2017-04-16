419b018e55a5d926eed0b710cb75c77e88fa59ee
BigEgg <cd_bigegg@hotmail.com>
2017-04-16 11:11:42 +0800

    Add the config document for Excel Report

---
+++ JsonComparer.Reports.Excel/Configurations/ExcelReportConfigurationDocument.cs
@@ -0,0 +1,98 @@
+﻿namespace BigEgg.Tools.JsonComparer.Reports.Excel.Configurations
+{
+    using ClosedXML.Excel;
+
+    internal class ExcelReportConfigurationDocument
+    {
+        public int SkipColumn { get { return 1; } }
+
+        public int SkipRow { get { return 1; } }
+
+        public StyleConfig Header1Style
+        {
+            get
+            {
+                return new StyleConfig()
+                {
+                    Bold = true,
+                    BackgroundColor = XLColor.CornflowerBlue,
+                    HorizontalAlignment = XLAlignmentHorizontalValues.Center
+                };
+            }
+        }
+
+        public StyleConfig Header2Style
+        {
+            get
+            {
+                return new StyleConfig()
+                {
+                    Bold = true,
+                    FontColor = XLColor.DarkBlue,
+                    BackgroundColor = XLColor.PaleAqua,
+                    HorizontalAlignment = XLAlignmentHorizontalValues.Center
+                };
+            }
+        }
+
+        public StyleConfig NewItemStyle
+        {
+            get
+            {
+                return new StyleConfig()
+                {
+                    FontColor = XLColor.DarkGreen,
+                    BackgroundColor = XLColor.LightGreen,
+                };
+            }
+        }
+
+        public StyleConfig RemovedItemStyle
+        {
+            get
+            {
+                return new StyleConfig()
+                {
+                    FontColor = XLColor.DarkRed,
+                    BackgroundColor = XLColor.Pink,
+                };
+            }
+        }
+
+        public StyleConfig DifferentItemStyle
+        {
+            get
+            {
+                return new StyleConfig()
+                {
+                    BackgroundColor = XLColor.IndianYellow,
+                };
+            }
+        }
+
+        public StyleConfig DifferentValueStyle
+        {
+            get
+            {
+                return new StyleConfig()
+                {
+                    Bold = true,
+                    FontColor = XLColor.Yellow,
+                    BackgroundColor = XLColor.IndianYellow,
+                };
+            }
+        }
+
+        public StyleConfig BorderStyle
+        {
+            get
+            {
+                return new StyleConfig()
+                {
+                    OutsideBorder = true,
+                    InsideBorder = true,
+                };
+            }
+        }
+    }
+}
---
+++ JsonComparer.Reports.Excel/Configurations/StyleConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace BigEgg.Tools.JsonComparer.Reports.Excel.Configurations
+{
+    using ClosedXML.Excel;
+
+    internal class StyleConfig
+    {
+        public bool? Bold { get; internal set; }
+
+        public XLColor FontColor { get; internal set; }
+
+        public XLColor BackgroundColor { get; internal set; }
+
+        public XLAlignmentHorizontalValues? HorizontalAlignment { get; internal set; }
+
+        public bool? OutsideBorder { get; internal set; }
+
+        public bool? InsideBorder { get; internal set; }
+    }
+}
