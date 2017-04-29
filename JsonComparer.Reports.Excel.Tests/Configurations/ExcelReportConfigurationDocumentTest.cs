3a4d2963ab02959f0e61049019e392280f4b1ae2
BigEgg <cd_bigegg@hotmail.com>
2017-04-29 00:04:29 -0700

    Add test for initial default excel report configuration document

---
+++ JsonComparer.Reports.Excel.Tests/Configurations/ExcelReportConfigurationDocumentTest.cs
@@ -0,0 +1,26 @@
+﻿namespace JsonComparer.Reports.Excel.Tests.Configurations
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using BigEgg.Tools.JsonComparer.Reports.Excel.Configurations;
+
+    [TestClass]
+    public class ExcelReportConfigurationDocumentTest
+    {
+        [TestMethod]
+        public void DefatulConfig()
+        {
+            var config = new ExcelReportConfigurationDocument();
+
+            Assert.AreEqual(1, config.SkipColumn);
+            Assert.AreEqual(1, config.SkipRow);
+            Assert.IsNotNull(config.Header1Style);
+            Assert.IsNotNull(config.Header2Style);
+            Assert.IsNotNull(config.NewItemStyle);
+            Assert.IsNotNull(config.RemovedItemStyle);
+            Assert.IsNotNull(config.DifferentItemStyle);
+            Assert.IsNotNull(config.DifferentValueStyle);
+            Assert.IsNotNull(config.BorderStyle);
+        }
+    }
+}
